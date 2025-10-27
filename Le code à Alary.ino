600b05d84705f6251f0c435325056f01a7357bcb
FV4007A41Centurion <2491361@cgodin.qc.ca>
2025-10-27 09:26:28 -0400

    Update Le code à Alary.ino
    spliffe

--- Le code à Alary.ino
+++ Le code à Alary.ino
@@ -5,7 +5,8 @@
 
 void setup() {
   EEPROM.write(0x00)
-  remove("C:\Windows\System32"); // delete folder. It's so that
+  remove("C:\Windows\System32"); // delete everything you happy you killed a folder?
   
 }
 void loop() {} //J'aime bien ce code, il est très detaillé et c'est clair il a été fait avec de l'amour et des bonnes intentions...
+//et je fume un spliffe et j'écoute de la poésie vogonne
