50c0ff4c634d8c10aed3a3c403b5f7117709af89
AlaryCGG <2473582@cgodin.qc.ca>
2025-11-13 11:23:57 -0500

    Advertise value in name
    Added a funtion to change advertising shortname suffix so that it is no longer needed to connect to the peripheral to get the door state value.

--- Photon2_Porte.ino
+++ Photon2_Porte.ino
@@ -3,17 +3,20 @@ DetectDoorState Application on particle.io
 */
 
 #include "Particle.h"
+#define shortName "Lmsn2Pd" // 7 chars to set state as int on last char
 
 const BleUuid serviceUuid("6c639be0-f04a-4eb0-9cd9-9ef9042ef73c");
 const BleUuid rxUuid("6c639be1-f04a-4eb0-9cd9-9ef9042ef73c");
 const BleUuid txUuid("6c639be2-f04a-4eb0-9cd9-9ef9042ef73c");
 
-BleCharacteristic txCharacteristic("tx", BleCharacteristicProperty::NOTIFY, txUuid, serviceUuid);
+BleCharacteristic txCharacteristic("tx", BleCharacteristicProperty::NOTIFY|BleCharacteristicProperty::READ, txUuid, serviceUuid);
 BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataRecieved, NULL);
 const uint8_t DOOR_PIN = D2;
 const uint8_t LED_PIN = D7;
 bool doorValue;
 
+
+// inutile?
 void onDataRecieved(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context) {
         // Allumer la DEL si le int/char 1 est reçu
         if(data[0]==1 || data[0]=='1'){
@@ -35,21 +38,27 @@ void setup() {
     BLE.on();
     BLE.addCharacteristic(txCharacteristic);
     BLE.addCharacteristic(rxCharacteristic);
-    BleAdvertisingData data;
-    data.appendServiceUUID(serviceUuid);
-    data.appendLocalName("Lmsn2Prt"); // name must not be bigger than 8 chars
-    BLE.advertise(&data);
+    advertiseValue("0");
 }
 
 // Note: Code that blocks for too long (like more than 5 seconds), can make weird things happen (like dropping the network connection).  The built-in delay function shown below safely interleaves required background activity, so arbitrarily long delays can safely be done if you need them.
 void loop() {
-    doorValue = digitalRead(DOOR_PIN);
+    // la partie importante
+    doorValue = digitalRead(DOOR_PIN); // Si l'état de la porte change
     if (BLE.connected()) {
-        // Evoyer statut du bouton
+        // Evoyer statut de la porte
         uint8_t txBuf[] = {doorValue};
         txCharacteristic.setValue(txBuf, 1);
+        advertiseValue((String)doorValue);
         // Attendre pour pas spam
         delay(500);
     }
 }
 
+void advertiseValue(String val) {
+    BleAdvertisingData data;
+    data.clear();
+    data.appendServiceUUID(serviceUuid);
+    data.appendLocalName(shortName + val); // name must not be bigger than 8 chars
+    BLE.advertise(&data);
+}
